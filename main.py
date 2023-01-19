c1ffef88054c68ad6ad34a1512ddbc2a328c946a
turko2373 <turko1073@gmail.com>
2023-01-19 03:10:22 -0300

    form design complete

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ template = Jinja2Templates(directory = "./view")
 @app.get("/")
 def root(req: Request):
     return template.TemplateResponse("index.html", {"request": req})
+
+
+@app.get("/signup")
+def signup(req: Request):
+    return template.TemplateResponse("signup.html", {"request": req})
+
+
+@app.get("/user")
+def user(req: Request):
+    return template.TemplateResponse("user.html", {"request": req})
+
